0c37ecc07b3b8c0107899ba45aa03ae0b331b1f0
Eliseo Orellana <eliseopatricio@gmail.com>
2024-05-18 23:04:04 -0400

    feat(class mapper):bookMapper

---
+++ src/main/java/io/github/eliseoorellana/LiterAlura/mapper/BookMapper.java
@@ -0,0 +1,36 @@
+package io.github.eliseoorellana.LiterAlura.mapper;
+
+import org.springframework.stereotype.Component;
+
+import io.github.eliseoorellana.LiterAlura.dto.BookDTO;
+import io.github.eliseoorellana.LiterAlura.model.Book;
+
+@Component
+public class BookMapper {
+    public BookDTO toDTO(Book book) {
+        if (book == null) {
+            return null;
+        }
+        BookDTO dto = new BookDTO();
+        dto.setId(book.getId());
+        dto.setTitle(book.getTitle());
+        dto.setAuthor(book.getAuthor());
+        dto.setLanguage(book.getLanguage());
+        dto.setDownloads(book.getDownloads());
+        return dto;
+    }
+
+    public Book toEntity(BookDTO dto) {
+        if (dto == null) {
+            return null;
+        }
+        Book book = new Book();
+        book.setId(dto.getId());
+        book.setTitle(dto.getTitle());
+        book.setAuthor(dto.getAuthor());
+        book.setLanguage(dto.getLanguage());
+        book.setDownloads(dto.getDownloads());
+        return book;
+    }
+
+}
