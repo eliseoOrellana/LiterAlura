c501a0fd611dd25d05ef5c9d47e96e4a5c6c2c9e
Eliseo Orellana <eliseopatricio@gmail.com>
2024-05-18 22:37:51 -0400

    feat(class bookDTO)

---
+++ src/main/java/io/github/eliseoorellana/LiterAlura/dto/BookDTO.java
@@ -0,0 +1,13 @@
+package io.github.eliseoorellana.LiterAlura.dto;
+
+import lombok.Data;
+
+@Data
+public class BookDTO {
+
+    private Long id;
+    private String title;
+    private String author;
+    private String language;
+    private int downloads;
+}
--- src/main/java/io/github/eliseoorellana/LiterAlura/model/Book.java
+++ src/main/java/io/github/eliseoorellana/LiterAlura/model/Book.java
@@ -1,4 +1,4 @@
-package model;
+package io.github.eliseoorellana.LiterAlura.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
