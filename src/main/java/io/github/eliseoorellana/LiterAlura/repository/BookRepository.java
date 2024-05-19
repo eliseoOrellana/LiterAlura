dc10a474c9442c111eca00adb4bf2d0a4c13f5a3
Eliseo Orellana <eliseopatricio@gmail.com>
2024-05-18 23:01:38 -0400

    feat(class BookRepository):implementation

--- src/main/java/io/github/eliseoorellana/LiterAlura/repository/BookRepository.java
+++ src/main/java/io/github/eliseoorellana/LiterAlura/repository/BookRepository.java
@@ -1,9 +1,14 @@
 package io.github.eliseoorellana.LiterAlura.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import io.github.eliseoorellana.LiterAlura.model.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long>  {
 
+    List<Book> findByTitleContaining(String title);
+    List<Book> findByLanguage(String language);
+    boolean existsByTitle(String title);
 }
